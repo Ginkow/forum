68077d08d6028a47f28d80d692c3fedaea4adec3
mjordan <jordan.milleville@ynov.com>
2024-04-26 09:23:50 +0200

    juste pour avoir les fichier

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	// "os"
+	// "os/exec"
+	"text/template"
+)
+
+func main() {
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+
+	// Demarrer le serveur
+	fmt.Println("Serveur écoutant sur le port 6969...")
+	http.ListenAndServe("localhost:6969", nil)
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	t, err := template.ParseFiles(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = t.Execute(w, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Menu(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "./index.html", nil)
+}
+
