696f2667f604ca1e6c7f143f53ee73957d2f4e98
mjordan <jordan.milleville@ynov.com>
2024-04-26 10:37:11 +0200

    modif

--- server.go
+++ server.go
@@ -1,37 +1,33 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	// "os"
-	// "os/exec"
-	"text/template"
+    "fmt"
+    "log"
+    "net/http"
+    "text/template"
 )
 
 func main() {
+    // Gestion des routes
+    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("./"))))
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+    // Gestion des fichiers statiques
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+    http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
 
-	// Demarrer le serveur
-	fmt.Println("Serveur écoutant sur le port 6969...")
-	http.ListenAndServe("localhost:6969", nil)
+    // Démarrer le serveur
+    fmt.Println("Serveur écoutant sur le port 6969...")
+    log.Fatal(http.ListenAndServe("localhost:6969", nil))
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	t, err := template.ParseFiles(tmpl)
-	if err != nil {
-		log.Fatal(err)
-	}
+    t, err := template.ParseFiles(tmpl)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	err = t.Execute(w, data)
-	if err != nil {
-		log.Fatal(err)
-	}
+    err = t.Execute(w, data)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
-
-func Menu(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "./index.html", nil)
-}
-
