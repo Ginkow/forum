92a8d765b512f66256418dc5ea4cfc97ff6295a7
mjordan <jordan.milleville@ynov.com>
2024-05-16 08:53:40 +0200

    test serv

--- server.go
+++ server.go
@@ -1,70 +1,50 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "net/http"
-    "text/template"
+	"fmt"
+	"log"
+	"net/http"
+	// "os"
+	// "os/exec"
+	"text/template"
 )
 
-var db *sql.DB
-
 func main() {
-    var err error
-    db, err = sql.Open("mysql", "user:password@/dbname")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
+    
+	http.HandleFunc("/", Menu)
 
     // Gestion des routes
-    http.HandleFunc("/register", RegisterHandler)
-    http.HandleFunc("/login", LoginHandler)
+    http.HandleFunc("/register", Register)
+    http.HandleFunc("/login", Login)
 
-    // Gestion des fichiers statiques
-    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-    http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./public/"))))
-
-    // Démarrer le serveur
-    fmt.Println("Serveur écoutant sur le port 6969...")
-    log.Fatal(http.ListenAndServe("localhost:6969", nil))
+	// Demarrer le serveur
+	fmt.Println("Serveur écoutant sur le port 6969...")
+	http.ListenAndServe("localhost:6969", nil)
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
     t, err := template.ParseFiles(tmpl)
     if err != nil {
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        log.Println(err)
-        return
+        log.Fatal(err)
     }
 
     err = t.Execute(w, data)
     if err != nil {
-        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-        log.Println(err)
+        log.Fatal(err)
     }
 }
 
-
-func RegisterHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        RenderTemplate(w, "/src/register.html", nil)
-        return
-    }
-
-    // Processus d'inscription
-    // Appelez la fonction RegisterHandler de votre package forum
-    RegisterHandler(w, r)
+func Menu(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "./index.html", nil)
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        RenderTemplate(w, "/src/login.html", nil)
-        return
-    }
+func Register(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "./src/register.html", nil)
+}
 
-    // Processus de connexion
-    // Appelez la fonction LoginHandler de votre package forum
-    LoginHandler(w, r)
+func Login(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "./src/login.html", nil)
 }
